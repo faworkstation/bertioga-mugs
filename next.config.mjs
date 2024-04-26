53e1315b5a271e5cb3a284007edd263ab9f4d6cc
faworkstation <felipeantoniati@gmail.com>
2024-04-26 09:38:02 -0300

    feat: ajustado o arquivo de configuração das imagens (next.config.js) para usar a propriedade remotePatterns em vez de domains.  a configuração domains, está obsoleta e não é mais recomendada para proteger contra solicitações maliciosas.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,13 @@
 const nextConfig = {
       reactStrictMode: true,
       images: {
-            domains: ['bertioga-mugs.s3.sa-east-1.amazonaws.com'],
+            remotePatterns: [
+                  {
+                        protocol: 'https',
+                        hostname: 'bertioga-mugs.s3.sa-east-1.amazonaws.com',
+                        port: '',
+                  },
+            ],
       },
 };
 
